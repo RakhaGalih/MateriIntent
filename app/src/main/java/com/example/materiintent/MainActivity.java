cc1f7459d1c302aa89d24e8637d30d4d1baff408
Rakha Galih <rakha_galih_28rpl@student.smktelkom-mlg.sch.id>
2021-02-09 12:17:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/materiintent/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.materiintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_DATA = "EXTRA_DATA";
+
+    private EditText editText;
+    private Button btnSendData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        btnSendData = (Button) findViewById(R.id.button);
+
+        btnSendData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String data = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, TargetActivity.class);
+                intent.putExtra(EXTRA_DATA, data);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
